ba56a758217f669a15abeeecbdcff5a2944d1e4c
Florriinnddaa <florinda.funya@gmail.com>
2026-09-22 00:40:42 +0200

    Configure MongoDB collection indexes

--- scripts/init-mongo.js
+++ scripts/init-mongo.js
@@ -1,9 +1,27 @@
-rs.initiate({
-  _id: "rs0",
-  members: [
-    {
-      _id: 0,
-      host: "mongodb:27017"
-    }
-  ]
-});
+db = db.getSiblingDB("customer_db");
+
+db.createCollection("customers");
+db.createCollection("restaurants");
+db.createCollection("orders");
+db.createCollection("payments");
+db.createCollection("drivers");
+db.createCollection("deliveries");
+db.createCollection("notifications");
+
+db.customers.createIndex(
+  { email: 1 },
+  { unique: true }
+);
+
+db.customers.createIndex(
+  { location: "2dsphere" }
+);
+
+db.restaurants.createIndex(
+  { location: "2dsphere" }
+);
+
+db.payments.createIndex(
+  { idempotencyKey: 1 },
+  { unique: true }
+);
